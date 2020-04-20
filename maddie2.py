028d646b5c6007ba7678c01aa251be72d001e3c2
harkano <harkano@gmail.com>
2020-04-20 12:30:18 +0100

    Add files via upload
    Changed some of the bot prefix stuff around.

--- maddie2.py
+++ maddie2.py
@@ -2,10 +2,12 @@
 # bot.py
 import os
 import discord
+from discord.ext import commands
 import random
 import json
 import re
 import logging
+#import owner
 
 from dotenv import load_dotenv
 
@@ -16,15 +18,60 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 load_dotenv()
-if __file__ == 'maddie2.py':TOKEN = os.getenv('DISCORD_TOKEN_DEV')
+print (os.path.basename(__file__))
+if os.path.basename(__file__) == 'maddie2.py':TOKEN = os.getenv('DISCORD_TOKEN_DEV')
 else:TOKEN = os.getenv('DISCORD_TOKEN')
+print (TOKEN)
+def get_prefix(bot, message):
+    if not message.guild:
+        return commands.when_mentioned_or("!")(bot, message)
+    with open("prefixes.json", 'r') as f:
+        prefixes = json.load(f)
+
+    if str(message.guild.id) not in prefixes:
+        return commands.when_mentioned_or("!")(bot, message)
+
+    prefix = prefixes[str(message.guild.id)]
+    return commands.when_mentioned_or(prefix)(bot,message)
+
 logger.info (TOKEN)
-client = discord.Client()
+#client = discord.Client()
+client = commands.Bot(command_prefix=get_prefix)
+
+async def is_guild_owner(ctx):
+    return ctx.author.id == ctx.guild.owner.id
+    print (ctx.author_id, ctx.guild.owner.id)
+
+
+async def prefix(self, ctx, *, pre):
+    with open(r"prefixes.json", 'r') as f:
+        prefixes = json.load(f)
+    prefixes[str(ctx.guild.id)] = pre
+    await ctx.send(f"New prefix is `{pre}`")
+
+    with open(r"prefixes.json", 'r') as f:
+        json.dump(prefixes, f, indent=4)
 
 ##Load in the existing moves
 input_file = open ('data.json')
 json_array = json.load(input_file)
 
+moves_list = []
+for p in json_array['moves']:
+    moves_list.append(p['shortName'])
+print (moves_list)
+
+def get_prefix(bot, message):
+    if not message.guild:
+        return commands.when_mentioned_or("!")(bot, message)
+    with open("prefixes.json", 'r') as f:
+        prefixes = json.load(f)
+    if str(message.guild.id) not in prefixes:
+        return commands.when_mentioned_or("!")(bot, message)
+    prefix = prefixes[str(message.guild.id)]
+    return commands.when_mentioned_or(prefix)(bot, message)
+
+        
 ##Setup the big sub
 def mad_parse(msg,user):
     blob = ""
@@ -101,33 +148,73 @@ async def on_ready():
     for x in range(len(servers)):
         logger.info('   ' + servers[x-1].name)
 
+
 #Listen for messages
 @client.event
 async def on_message(message):
+    pre = get_prefix
+    print (pre)
     if message.author == client.user:
         return
     #answer a call for help
     elif message.content.startswith("!help"):
         log_line = message.guild.name + "|" + message.channel.name + "|" + message.author.name + "|" + message.content
         logger.info(log_line)
-        help_file = open("help","r")
-        response = help_file.read() 
+        help_file = open("../../aws/help", "r")
+        response = help_file.read()
         await message.channel.send(response)
     #list moves
     elif message.content.startswith("!moves+"):
         response = '**Name - description, keyword, label**\n'
         for p in json_array['moves']:
             response = response + p['capital'].capitalize() + " - " + p['description'] + ", " + p['shortName'] + ", " + p['label'] + "\n "
-        await message.channel.send(response) 
+        await message.channel.send(response)
     elif message.content.startswith("!moves"):
         response = ''
         for p in json_array['moves']:
             response = response + p['shortName'] + ", "
-        await message.channel.send(response) 
+        await message.channel.send(response)
 #remember generic ! should always be last in the tree
     elif message.content.startswith("!"):
         log_line = message.guild.name + "|" + message.channel.name + "|" + message.author.name + "|" + message.content
         logger.info(log_line)
         response =  mad_parse(message.content, message.author.display_name)
         if response: await message.channel.send(embed=response)
+
+    await client.process_commands(message)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@client.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+@client.command()
+async def moves(ctx):
+    response = ''
+    for p in json_array['moves']:
+        response = response + p['shortName'] + ", "
+    await ctx.send(response)
+
+#Leaving this out for now as i like the message regex match for being more flexible, but this would be the right way to do it
+#@client.command(aliases=moves_list)
+#async def move(ctx, *, words):
+#    print (words, ctx)
+#    await ctx.send(embed=mad_parse(ctx.c, ctx.author.display_name))
+
+@client.command()
+@commands.is_owner()
+async def prefix(ctx, *, pre):
+    with open(r"prefixes.json", 'r') as f:
+        prefixes = json.load(f)
+    prefixes[str(ctx.guild.id)] = pre
+    await ctx.send(f"New prefix is `{pre}`")
+
+    with open(r"prefixes.json", 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+
+#runs the bot!
 client.run(TOKEN)
