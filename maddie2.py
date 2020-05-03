37e9f5553a592a9d1ad5e3cc57c625b5695f1dde
Gonzalo Hernandorena <gon@localhost.localdomain>
2020-05-03 17:39:03 -0300

    Added refactor already applied to maddie

--- maddie2.py
+++ maddie2.py
@@ -14,6 +14,7 @@ from utils import get_modified_num
 from moves import get_moves
 from playbook_interactions import lock_label, edit_labels, mark_potential, mark_condition, clear_condition
 from command_handler import plain_command_handler, embed_command_handler
+from parse import mad_parse, add_result
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO) #set logging level to INFO, DEBUG if we want the full dump
@@ -67,84 +68,6 @@ for p in json_array['moves']:
 print (moves_list)
 
 
-def add_result (embed, num_calc, mod):
-    #do dice rolling
-    result1 = random.randrange(1,6) ##first d6
-    result2 = random.randrange(1,6) ##second d6
-    result_tot = result1 + result2 + num_calc #2 d6 + mod
-
-    if mod == '-':
-        modifier_to_show = ''
-    else:
-        modifier_to_show = f' {mod}'
-
-    embed.add_field(name="Calculation", value=f"Dice **{result1}** + **{result2}**, Label{modifier_to_show} **{num_calc}**", inline=False)
-    embed.add_field(name="Result", value=f"**{result_tot}**")
-
-
-##Setup the big sub
-def mad_parse(msg,user):
-    blob = ""
-    capital = ""
-    phrase = ""
-    word = ""
-    mod = ""
-    num = int(0)
-    quiet = 0
-    match = 0
-    # parse command into parts
-    msg = msg.lower()
-    searchStr1 = r'[a-z]+'
-    searchStr2 = r'[\+\-]'
-    searchStr3 = r'[1234567890]'
-    log_line = ''
-    result1 = re.search(searchStr1, msg)
-    if result1:
-        log_line = log_line + result1.group(0)
-        word = result1.group(0)
-    else: log_line = log_line + "no cmd "
-    result2 = re.search(searchStr2, msg)
-    if result2:
-        log_line = log_line + result2.group()
-        mod = result2.group(0)
-    else: log_line = log_line + "no mod "
-    result3 = re.search(searchStr3, msg)
-    if result3:
-        log_line = log_line + result3.group(0)
-        num = int(result3.group(0))
-    else: log_line = log_line + "no num "
-    logger.info (msg + log_line)
- # figure out which type of modifier it is
-    num_calc = get_modified_num(mod, num)
- # lookup a table for the big blob of text and a wee blob
-    for p in json_array['moves']:
-        if p['shortName'] == word:
-            blob = p['blob']
-            capital = p['capital']
-            phrase = p['phrase']
-            img = p['img']
-            roll = p['requiresRolling']
-            match = 1
-    #Quiet mode
-    searchStr4 = r'!!'
-    result4 = re.search(searchStr4, msg)
-    if result4: quiet = 1
-
-#Ugly format blob!
-    if match == 1 : #lets us ignore ! prefix commands that aren't in our list
-        embed=discord.Embed(title=f"{capital}")
-        embed.set_author(name=f"{user} {phrase}")
-        embed.set_thumbnail(url=img)
-        if quiet == 0: embed.add_field(name="Description", value=f"{blob}") # don't include the blob if we're in quiet mode (!!)
-        if roll:
-            add_result(embed, num_calc, mod)
-        embed.set_footer(text=" ")
-
-        return embed
-
-    else:return 0
-
-
 @client.event
 async def on_ready():
     logger.info(f'{client.user} has connected to Discord!')
@@ -167,13 +90,13 @@ async def on_message(message):
         await message.channel.send(move_list)
         return
     # handle help and all of the playbook interactions
-    response = plain_command_handler(message)
+    response = plain_command_handler(message, 'en')
 
     if response:
         await message.channel.send(response)
         return
 
-    response = embed_command_handler(message)
+    response = embed_command_handler(message, 'en')
 
     if response:
         await message.channel.send(embed=response)
