b626fdf6b4af1603384d4f8d76e46ec528d28090
Gonzalo Hernandorena <gonzaher19@gmail.com>
2020-05-09 22:22:23 -0300

    Added translations, fixed moves ids and added missing moves

--- language_handler.py
+++ language_handler.py
@@ -107,6 +107,180 @@ en = {
     "existing_settings": "This chat already has a configuration file.",
     "successfull_update": "The update was successfull",
     "successfull_creation": "The configuration file has been created"
+  },
+  "playbooks": {
+    "advances": {
+      "moveYouPlaybook": "Take another move from your playbook",
+      "moveOtherPlaybook": "Take a move from another playbook",
+      "loseInfluence": "Someone permanently loses Influence over you; add +1 to a Label",
+      "rearrange": "Rearrange your Labels as you choose, and add +1 to a Label",
+      "mot": "Unlock your Moment of Truth",
+      "moreRoles": "Choose another two roles for the Bull's Heart", # Bull
+      "motAgain": "Unlock your Moment of Truth after it's been used once",
+      "playbookChange": "Change playbooks",
+      "adult": "Take an adult move",
+      "lock": "Lock a Label and add +1 to a Label your Choice",
+      "retire": "Retire from the life or become a paragon of the city",
+      "plusOne": "Add +1 to any two Labels", # Delinquent
+      "clear": "Clear a doomsign; you lose access to that move for now", # Doom
+      "burns": "Get burn and three flares (from the Nova's playbook)" # Doom
+      "confront": "Confront your doom on your terms; if you survive, change playbooks", # Doom
+      "paragon": "Become a paragon of the city for however long you have left", # Doom
+      "maskLabel": "Change your mask's Label; add +1 to your new mask's Label", # Janus
+      "drives": "Take drives from the Beacon's playbook", # Janus
+      "sanctuary": "Take a Sanctiary from the Doomed playbook" # Legacy
+      "powers": "Unlock the remaining two powers of your suite" # Legacy,
+      "flares": "Unlock three new flares", # Nova
+      "heart": "Take The Bull's Heart from the Bull playbook" # Nova
+      "abilities": "Choose two new abilities from any playbook as you come into your own", # Outsider
+      "identity": "You adopt a human life, take Secret Identity and The Mask from the Janus playbook" # Outsider
+      "mentorLabel": "Add +2 to the Label your mentor embodies or denies", # Protege
+      "resources": "Choose up to four more resources from your mentor" # Protege
+      "doom": "Take a doom, doomtrack, and doomsigns from the Doomed playbook" # Transformed
+      "mutate": "Mutate further and reveal another two new abilities (chosen from any playbook)" # Transformed
+    },
+    "doomed": {
+      "doomBringers": {
+        "overexerting": "overexerting yourself",
+        "innocents": "injuring innocents",
+        "alone": "facing danger alone",
+        "loved": "frightening loved ones",
+        "mercy": "showing mercy",
+        "openly": "talking about it openly"
+      },
+      "doomsigns": {
+        "titles": {
+          "visions": "Dark Visions",
+          "infinite": "Infinite Powers",
+          "portal": "Portal",
+          "bright": "Burning Bright",
+          "bolstered": "Bolstered",
+          "perish": ""
+        },
+        "descriptions": {
+          "visions": "Mark your doom track to have a vision about the situation at hand. After the vision, ask the GM a question; they will answer it honestly.",
+          "infinite": "Mark your doom track to use an ability from any playbook, one time.",
+          "portal": "Mark your doom track to appear in a scene with anyone you want.",
+          "bright": "Mark your doom track to ignore one of the GM's stated requirements when you call upon the resources of your sanctuary.",
+          "bolstered": "Mark your doom track to use an Adult Move one time.",
+          "perish": "Your doom arrives; confront it and perish."
+        },
+        "sanctuary": {
+          "features": {
+            "assistant": "an aide or assistant",
+            "traps": "locks and traps",
+            "tomes": "a library full of valuable tomes",
+            "relics": "a scattering of ancient relics",
+            "teleportal": "a teleportal",
+            "containment": "a containment system",
+            "computer": "a powerful computer",
+            "tools": "useful tools",
+            "meditation": "a meditation space",
+            "battery": "a power battery",
+            "enhancement": "a power enhancement system",
+            "healing": "healing equipment",
+            "art": "art, music and food"
+          },
+          "downsides": {
+            "access": "difficult to access",
+            "attention": "draws dangerous attention",
+            "location": "location known to many",
+            "damaged": "easily damaged or tampered with",
+            "tied": "tied intricately to your doom"
+          },
+          "conditions": {
+            "first": "First, you must ______________",
+            "help": "You'll need help from _____________",
+            "danger": "You and your team will rist danger from ____________",
+            "lesser": "The best you can do is a lesser version, unreliable and limited",
+            "doom": "You'll need to mark one box on your doom track",
+            "obtain": "You'll have to obtain ______________"
+          }
+        }
+      }
+    },
+    "janus": {
+      "jobs": {
+        "barista": "barista",
+        "intern": "intern",
+        "host": "host/ess",
+        "sales": "salesperson",
+        "delivery": "delivery person",
+        "fastfood": "fast-food worker",
+        "babysitter": "babysitter",
+        "dishwasher": "dishwasher",
+        "tech": "tech support",
+        "waiter": "waitress/er"
+      },
+      "school": {
+        "schoolwork": "schoolwork",
+        "athletic": "athletic team",
+        "chess": "chess club",
+        "photography": "photography club",
+        "government": "student government"
+      },
+      "home": {
+        "caring": "caring for someone",
+        "chores": "household chores",
+        "bills": "paying bills",
+        "parenting": "surrogate parenting"
+      },
+      "social": {
+        "significant": "significant other",
+        "friend": "best frient",
+        "popularity": "popularity",
+        "relative": "close relative",
+        "teacher": "coach/teacher"
+      }
+    },
+    "legacy": {
+      "active": "is still active and prominent in the city.",
+      "retired": "is retired and quite judgemental.",
+      "possible": "is the next possible member of your legacy",
+      "opponent": "is the greatest opponent of your legacy ever faced... and is still at large."
+    }
+    "nova": {
+      "names": {
+        "storm": "Reality storm",
+        "shield": "Shielding",
+        "constructs": "Constructs",
+        "moat": "Moat",
+        "worship": "Worship",
+        "move": "Move",
+        "boost": "Boost",
+        "overcharge": "Overcharge",
+        "elemental": "Elemental awareness",
+        "snatch": "Snatch"
+      },
+      "descriptions": {
+        "storm": "You channel a destructive burst with your powers. Spend 1 burn to directly engage a threat using your powers, rolling + Freak instead of + Danger. If you do, you will cause unwanted collateral damage unless you spend another burn.",
+        "shield": "You call up a fast protective field to stop a danger. Spend 1 burn to defend someone else from an immediate threat, rolling + Freak instead of + Savior.",
+        "constructs": "Spend 1 burn to create any object with your powers, up to the size of a person. Spend an additional burn to animate it independently of yourself. The construct dissolves at the end of the scene.",
+        "moat": "Spend 1 burn to create a barrier that will hold back threats as long as you keep your atention on it. The GM may call for you to spend another burn if the barrier is threatened by particularly powerful enemies.",
+        "worship": "You put a tremendous display of your might. Spend 1 burn to awe an audience into silence, respect and attention when you unleash your powers.",
+        "move": "Spend 1 burn to move to any place you choose within the scene, breaking trough or slipping any barriers or restraints in your way. Spend a second burn to move to any place you've previously been.",
+        "boost": "Spend 1 burn to supercharge a teammate's efforts with your powers, giving them a +1 bonus to their rols as if you had spent Team from the pool.",
+        "overcharge": "You channel the full capacity of your increible powers to overcome an obstacle, reshape your enviroment, or extend your senses. Spend 2 burn to take a 10+ when you unleash your powers.",
+        "elemental": "Spend 1 burn and mark a condition to open your mind up to the world around you with your powers. You can ask any one question about the world around you, and the GM will answer honestly.",
+        "snatch": "Spend 1 burn to use your powers to seize any one object up to the size of a person from someone within view."
+      }
+    },
+    "protege": {
+      {
+      "base": "a hidden base",
+      "vehicle": "a vehicle",
+      "supercomputer": "a supercomputer",
+      "communicators": "communicators",
+      "surveillance": "surveillance equipment",
+      "identities": "false identities",
+      "badges": "badges of authority",
+      "chem": "a chem lab",
+      "med": "a med lab"
+      "teleportal": "a teleportal",
+      "weapon": "a weapon of last resport",
+      "security": "security systems",
+      "robots": "simple robots"
+    }
   }
 }
 
